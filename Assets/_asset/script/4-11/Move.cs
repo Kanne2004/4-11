0406f50741e105d2dbb37be03ad2be1b3be99ca5
Kanne <doantrungkien18@gmail.com>
2024-11-06 16:38:22 +0700

    update 6/11

--- Assets/_asset/script/4-11/Move.cs
+++ Assets/_asset/script/4-11/Move.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float speed = 0.01f;
+    public float jump = 3;
+    public Animator anim;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
+            anim.SetFloat("move", 1);
             transform.position += new Vector3(-1 * speed, 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
+            anim.SetFloat("move", 1);
             transform.position += new Vector3(1 * speed, 0, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.A)|| Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetFloat("move", 0);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position += new Vector3(0, 1, 0);
+            anim.SetFloat("jump", 1);
+            transform.position += new Vector3(0, jump , 0);
+            
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            transform.position += new Vector3(0, 1, 0);
+            anim.SetFloat("jump", 0);
         }
     }
 }
